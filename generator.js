53d6b1620c707b63043cf3ca2eaf5fe642e423af
Hrishi Patil <24883092+huskynp@users.noreply.github.com>
2022-07-03 20:01:13 -0400

    added known death/alive in year filter, back button

--- generator.js
+++ generator.js
@@ -1,8 +1,11 @@
+let last = [];
+let c_doc = undefined;
+
 const IDtoImage = (id, emojis) => {
     $('#card > img').hide();
     $.get(`https://www.wikidata.org/w/api.php?action=wbgetclaims&format=json&property=P18&origin=*&entity=${id}`)
     .then(data =>{
-        if(data.claims.P18) $('#card > img').attr('src', `https://commons.wikimedia.org/w/index.php?title=Special:Redirect/file/${data.claims.P18[0].mainsnak.datavalue.value}&width=100`);
+        if(data.claims.P18 && !data.error) $('#card > img').attr('src', `https://commons.wikimedia.org/w/index.php?title=Special:Redirect/file/${data.claims.P18[0].mainsnak.datavalue.value}&width=100`);
         else if(emojis.length > 0) $('#card > img').attr('src', `https://emojicdn.elk.sh/${emojis[0]}?style=twitter`); 
         else $('#card > img').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg');
         $('#card > img').show();
@@ -33,19 +36,35 @@ const setCard = (doc) => {
     $("#article").attr('aria-busy', false);
     IDtoImage(doc.id, emojis);
     $('#card > h2').text(doc.name);
-    $('#card > #date').text(`${doc.born} - ${doc.died || ''} ${emojis.join(' ')}`);
+    $('#card > #date').text(`${(doc.born < 0 ? `${doc.born*-1} BCE` : doc.born)} - ${(doc.died < 0 ? `${doc.died*-1} BCE` : (doc.born < 0 ? `${doc.died} CE` : doc.died)) || ''} ${emojis.join(' ')}`);
     $('#card > p').text(removeDate(doc.description));
-    $("#card > #death-reason").text(`cause of death: ${doc['death-reason'] || 'unkown'}`);
-    $("#card > a").attr('href', `https://wikidata.org/wiki/${doc.id}`);
+    $("#card > #death-reason").text(`cause of death: ${doc['death-reason'] || 'unknown'}`);
+    $("#card > #link").attr('href', `https://wikidata.org/wiki/${doc.id}`);
+    if(last.length !== 0) $("#card > #back").attr("aria-disabled", false);
     $("#card").show()
 }
 
 $("#main").click(async function(){
+    if(c_doc) last.push(c_doc);
     $("#card").hide();
     $("#article").attr('aria-busy', true);
-    const docs = await collection.aggregate([{ $sample: { size: 1 } }]);
-    const doc = docs[0];
-    setCard(doc);
+    let queries = [{ $sample: { size: 1 } }];
+    if($("#known-cod>input").is(':checked')) queries.unshift({$match: { 'death-reason': { $ne : '' }}});
+    if($("#year>input").val() !== '') 
+        queries.unshift({$match: { 'died': { $ne : '' }}});
+        queries.unshift({$match: {$expr: {$gte: [{$toInt: "$died"}, parseInt($("#year>input").val())]}}});
+        queries.unshift({$match: {$expr: {$lte: [{$toInt: "$born"}, parseInt($("#year>input").val())]}}});
+    const docs = await collection.aggregate(queries);
+    c_doc = docs[0];
+    if(c_doc) setCard(c_doc);
+    else alert("couldn't find any person alive in that year and/or with a cause of death.");
+
+});
+
+$("#back").click(function(){
+    setCard(last[last.length-1]);
+    last.pop();
+    if(last.length === 0) $("#card > #back").attr("aria-disabled", true);
 });
 
 $("#card").hide();
